0037cbdfe949718401a11d035574e9dd576671b4
David McKee <git-author.n4t20@simplelogin.com>
2026-04-05 11:28:04 -0400

    waybar: auto-approve pacman upgrade, echo command before sudo prompt

--- .config/waybar/scripts/update-packages.sh
+++ .config/waybar/scripts/update-packages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Run pacman -Syu in a terminal, then signal waybar to refresh the updates module
-alacritty -e sudo pacman -Syu
+alacritty -e bash -c 'echo "Running: sudo pacman -Syu --noconfirm"; sudo pacman -Syu --noconfirm'
 pkill -SIGRTMIN+8 waybar
